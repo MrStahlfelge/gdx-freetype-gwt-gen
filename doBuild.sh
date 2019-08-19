3e0f3d2259b1c1d1b2b039421af161008d2940a5
intrigus <abc123zeus@live.de>
2019-08-19 19:59:50 +0200

    Finally?

---
+++ doBuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp freetype-2.9.1 freetype-2.9.1-native
+cd freetype-2.9.1-native
+./autogen.sh
+./configure
+make
+cd ..
+cp freetype-2.9.1-native/objs/apinames freetype-2.9.1/objs/apinames
+rm -r freetype-2.9.1-native
+cd freetype-2.9.1
+./autogen.sh
+emconfigure ./configure
+emmake make
+emcc -I ./include -o ../freetype.o ../freetype.c
+emcc -O3 --llvm-lto 1 -s ALLOW_MEMORY_GROWTH=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=[\"writeArrayToMemory\"]' -s NO_EXIT_RUNTIME=1 -s AGGRESSIVE_VARIABLE_ELIMINATION=1 -s NO_DYNAMIC_EXECUTION=1 --memory-init-file 0 -s NO_FILESYSTEM=1 -s NO_BROWSER=1 -s ../freetype.o objs/.libs/libfreetype.a -o ../freetype.js
